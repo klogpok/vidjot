2f42cf17a328d20f23dd7e79d8a2ae91f2584f3b
klogpok <kloleg@gmail.com>
2018-05-19 16:20:27 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+const port = 5000;
+const app = express();
+
+// Connect to Mongoose
+mongoose
+  .connect('mongodb://localhost/vidjot-dev', {
+    //useMongoClient: true
+  })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// Load Ideas Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'index' }));
+app.set('view engine', 'handlebars');
+
+// Create Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome!';
+  res.render('index', { title });
+});
+
+// Create About Route
+app.get('/about', (req, res) => {
+  const title = 'About';
+  res.render('about', { title });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
