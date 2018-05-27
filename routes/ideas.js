86161888a9cd72114b79a56cd6541bfbba7409b8
klogpok <kloleg@gmail.com>
2018-05-27 17:30:31 +0300

    Protecting Routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const { ensureAuthenticated } = require('../helpers/auth');
 
 // Load Ideas Model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
 // Ideas Index page
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
   Idea.find({})
     .sort({ date: 'desc' })
     .then(ideas => {
@@ -16,12 +17,12 @@ router.get('/', (req, res) => {
 });
 
 // Add Idea Form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('ideas/add');
 });
 
 // Edit Idea Form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
@@ -30,7 +31,7 @@ router.get('/edit/:id', (req, res) => {
 });
 
 // Process Form
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
   let errors = [];
 
   if (!req.body.title) {
@@ -61,7 +62,7 @@ router.post('/', (req, res) => {
 });
 
 // Edit Form Process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
@@ -76,7 +77,7 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete Idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
   Idea.remove({ _id: req.params.id }).then(() => {
     req.flash('success_msg', 'Video idea removed');
     res.redirect('/ideas');
