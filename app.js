5a5ea6c1f05a2d2b8f6b1f868a5be276c1c6d605
klogpok <kloleg@gmail.com>
2018-05-27 18:24:53 +0300

    fix port

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use('/ideas', ideas);
 // Use Users routes
 app.use('/users', users);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
